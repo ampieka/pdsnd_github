1cfb69ea1a2dddcf53747a37caa57e2e613f13d6
ampieka <57642439+ampieka@users.noreply.github.com>
2025-07-26 13:05:54 -0400

    making more changes to simulate others making changes

--- bikeshare.py
+++ bikeshare.py
@@ -2,7 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 import datetime
-
+'''making a change because i have to'''
 CITY_DATA = {'chicago': 'chicago.csv',
              'new york city': 'new_york_city.csv',
              'washington': 'washington.csv'}
